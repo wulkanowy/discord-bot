026237337d70116e87cbccf48be6335934d00479
Dominik Korsa <dominik.korsa@gmail.com>
2018-12-11 20:52:50 +0100

    Added greeting

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,9 @@
 module.exports = (client) => {
+  client.guilds.forEach(guild => {
+    console.log(`Dostępny na ${guild.name}`);
+    const channel = guild.channels.find(ch => ch.name === client.config.channel);
+    if (!channel) return;
+    channel.send('Witam, jestem!');
+  });
   console.log(`Uruchomiono bota :)`);
 }
