58368b637e91f7c366b4a02536f7cd9c22da573a
Dominik Korsa <dominik.korsa@gmail.com>
2018-12-13 17:20:46 +0100

    Added channels object in config

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,7 @@ var Discord = require("discord.js");
 module.exports = (client) => {
   client.guilds.forEach(guild => {
     console.log(`Dostępny na ${guild.name}`);
-    const channel = guild.channels.find(ch => ch.name === client.config.channel);
+    const channel = guild.channels.find(ch => ch.name === client.config.channels.bot);
     if (!channel) return;
     const embed = new Discord.RichEmbed()
       .setAuthor("Witam, jestem!", "https://doteq.pinglimited.me/515xf8.png")
