d00314e1bf846533ad4f1c615bc4b549832ff53d
Dominik Korsa <dominik.korsa@gmail.com>
2018-12-13 19:31:27 +0100

    Formatted some files

--- commands/pobierz.js
+++ commands/pobierz.js
@@ -3,10 +3,8 @@ exports.run = (client, message, args) => {
   const embed = new Discord.RichEmbed()
       .setAuthor("Pobierz Wulkanowy!", "https://doteq.pinglimited.me/515xf8.png")
       .setColor("F44336")
-      .addField("Google Play:",
-          "https://play.google.com/store/apps/details?id=io.github.wulkanowy")
-      .addField("Wersję beta/dev pobierzesz na:",
-          "https://wulkanowy.github.io/")
+      .addField("Google Play:", "https://play.google.com/store/apps/details?id=io.github.wulkanowy")
+      .addField("Wersję beta/dev pobierzesz na:", "https://wulkanowy.github.io/")
       .setFooter("Wygenerowano przez Wulkanowy Bot", "https://doteq.pinglimited.me/515xf8.png");
   message.channel.send({embed});
 }
--- commands/status.js
+++ commands/status.js
@@ -1,12 +1,12 @@
 const getTitleAtUrl = require('get-title-at-url');
 exports.run = (client, message, args) => {
-getTitleAtUrl("https://uonetplus-uczen.vulcan.net.pl/", function(title){
-          if (title === "Przerwa techniczna"){
-message.channel.send(`Dzienniczek Vulcan przeszedł na "przerwę techniczną", czyli tak naprawdę ma awarię. Więc aplikacja też.`);
-          }
-          else{
-			  message.channel.send("Dzienniczek działa poprawnie.");
-			  laststatus = true;
-		  }
-});
+    getTitleAtUrl("https://uonetplus-uczen.vulcan.net.pl/", function(title){
+    if (title === "Przerwa techniczna"){
+      message.channel.send(`Dzienniczek Vulcan przeszedł na "przerwę techniczną", czyli tak naprawdę ma awarię. Więc aplikacja też.`);
+    }
+    else {
+      message.channel.send("Dzienniczek działa poprawnie.");
+      laststatus = true;
+    }
+  });
 }
