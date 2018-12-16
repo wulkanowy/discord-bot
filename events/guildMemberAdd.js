0db8146d8ddc4ec1ddb7cfe7be79bc23ad92cec8
Dominik Korsa <dominik.korsa@gmail.com>
2018-12-16 11:27:08 +0100

    Changed user wellcome message

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -4,8 +4,8 @@ module.exports = (client, member) => {
   var avatar = member.user.displayAvatarURL;
   var id = member.user.id;
   var embedDM = new Discord.RichEmbed()
-    .setTitle(`Witamy na serwerze`)
-    .setDescription("Aby skorzystać z bota wpisz `!pomoc` na kanale #bot\nAby dostać rolę wpisz `!rola dodaj <nazwa>`\nAby uzyskać listę ról wpisz `!rola lista`")
+    .setTitle(`Witaj na serwerze Wulkanowego!`)
+    .setDescription("Używany tutaj specjalych ról, by móc oznaczyć osoby korzystające z danej wersji e-dziennika (np. `vulcan.net.pl`, `eszkola.opolskie.pl`).\nUłatwia to rozwiązywanie problemów i testowanie nowych funkcji.\n\n**UWAGA**: Wszystkie komendy wpisuje się na kanale `#bot`\n\n - By wyświetlić listę wszystkich ról, wpisz ```!rola lista```\n - By przydzielić sobie odpowienią rolę, na kanale #bot wpisz ```!rola dodaj <nazwa_roli>```Na przykład\n```!rola dodaj vulcan.net.pl```\n - By wyświetlić listę wszystkich komend obsługiwanych przez naszego bota, wpisz ```!pomoc```\n\n**Dziękujemy za wybranie naszej aplikacji!**")
     .setColor("F44336");
   member.send({embed: embedDM});
 
