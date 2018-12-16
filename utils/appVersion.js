d65c10745badc8c8fcec3518f30c2d29b2e9dbac
Dominik Korsa <dominik.korsa@gmail.com>
2018-12-16 13:07:29 +0100

    Added getDevBuild to appVersion

--- utils/appVersion.js
+++ utils/appVersion.js
@@ -44,3 +44,48 @@ module.exports.getBetaBuild = function () {
     });
   });
 }
+
+module.exports.getDevBuild = function () {
+  return new Promise((resolve, reject) => {
+    https.get("https://bitrise-redirector.herokuapp.com/v0.1/apps/daeff1893f3c8128/builds/master/artifacts/0/info", function(res) {
+      var body = '';
+      
+      let error;
+      if (res.statusCode !== 200) {
+        error = new Error(`Request Failed. Status Code: ${res.statusCode}`);
+      } else if (!/^application\/json/.test(res.headers['content-type'])) {
+        error = new Error(`Invalid content-type. Expected application/json but received ${res.headers['content-type']}`);
+      }
+
+      if (error) {
+        reject(error);
+        res.resume();
+        return;
+      }
+
+      res.on('data', function(chunk){
+        body += chunk;
+      });
+      
+      res.on('end', function(){
+        try {
+          var response = JSON.parse(body);
+          console.log("Response: ", response);
+          resolve ({
+            url: response.public_install_page_url,
+            directUrl: response.expiring_download_url,
+            version: response.build_number
+          });
+        }
+        catch (error) {
+          console.log(body);
+          reject(error);
+          return;
+        }
+      });
+    }).on('error', function(error){
+      reject(error);
+      return;
+    });
+  });
+}
