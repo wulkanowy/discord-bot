f3a47cb290ffea247110812ff11b67a8ba6e7f8a
doteq <dawidwikus@gmail.com>
2018-12-18 13:18:01 +0100

    Added old module and embed

--- commands/status.js
+++ commands/status.js
@@ -1,14 +1,79 @@
 const getTitleAtUrl = require('get-title-at-url');
+var Discord = require("discord.js");
+var stary;
+var nowy;
 exports.run = (client, message, args) => {
-  getTitleAtUrl("https://uonetplus-uczen.vulcan.net.pl/", function(title){
-    if (title === "Przerwa techniczna"){
-      message.channel.send(`Dzienniczek Vulcan przeszedł na "przerwę techniczną", czyli tak naprawdę ma awarię. Więc aplikacja też.`);
-    }
-    else {
-      message.channel.send("Dziennik działa poprawnie", {
-        files: ['https://i.imgur.com/FcPd2Nf.png']
-      });
-      laststatus = true;
-    }
-  });
+getTitleAtUrl("https://uonetplus-uczen.vulcan.net.pl/", function(title){
+          if (title === "Przerwa techniczna"){
+nowy = false;
+          }
+          else{
+			  nowy = true;
+		  }
+});
+getTitleAtUrl("https://uonetplus-opiekun.vulcan.net.pl/", function(title){
+          if (title === "Przerwa techniczna"){
+stary = false;
+console.log("nie dziala")
+          }
+          else{
+			  stary = true;
+			  console.log("Dziennik dziala");
+		  }
+});
+
+message.channel.send("Sprawdzam... (To potrwa 5 sekund)")
+    .then((message) => {
+      message.delete(5000)
+      .catch((error) => {});
+    });
+
+setTimeout(function() {
+
+if(nowy && stary){
+	const embed = new Discord.RichEmbed()
+      .setTitle("Status dziennika")
+      .setColor("2ecc71")
+      .addField("Nowy moduł uczeń:",
+          "Wszystko powinno działać poprawnie.")
+      .addField("Stary moduł uczeń:",
+          "Wszystko powinno działać poprawnie.")
+      .setFooter("Wygenerowano przez Wulkanowy Bot", "https://doteq.pinglimited.me/515xf8.png");
+  message.channel.send({embed});
+}
+else if(nowy && !stary) {
+	const embed = new Discord.RichEmbed()
+      .setTitle("Status dziennika")
+      .setColor("f1c40f")
+      .addField("Nowy moduł uczeń:",
+          "Wszystko powinno działać poprawnie.")
+      .addField("Stary moduł uczeń:",
+          "Awaria")
+      .setFooter("Wygenerowano przez Wulkanowy Bot", "https://doteq.pinglimited.me/515xf8.png");
+  message.channel.send({embed});
+}
+else if(!nowy && stary){
+	const embed = new Discord.RichEmbed()
+      .setTitle("Status dziennika")
+      .setColor("f1c40f")
+      .addField("Nowy moduł uczeń:",
+          "Awaria")
+      .addField("Stary moduł uczeń:",
+          "Wszystko powinno działać poprawnie.")
+      .setFooter("Wygenerowano przez Wulkanowy Bot", "https://doteq.pinglimited.me/515xf8.png");
+  message.channel.send({embed});
+}
+else{
+	console.log(nowy + stary)
+	const embed = new Discord.RichEmbed()
+      .setTitle("Status dziennika")
+      .setColor("e74c3c")
+      .addField("Nowy moduł uczeń:",
+          "Awaria")
+      .addField("Stary moduł uczeń:",
+          "Awaria")
+      .setFooter("Wygenerowano przez Wulkanowy Bot", "https://doteq.pinglimited.me/515xf8.png");
+  message.channel.send({embed});
+}
+}, 5000);
 }
