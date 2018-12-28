a741a0c2db5f918ef5959d66711fa168fb702a97
Dominik Korsa <dominik.korsa@gmail.com>
2018-12-28 14:40:45 +0100

    Added status errors support and changed color logic

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,8 @@ const Discord = require("discord.js");
 const uonetStatus = require("../utils/uonetStatus");
 
 var statusChannels = [];
-var lastStatusCode = 3;
+var lastStatusCode = 0;
+
 module.exports = (client) => {
   client.guilds.forEach(guild => {
     console.log(`Dostępny na ${guild.name}`);
@@ -34,16 +35,42 @@ module.exports = (client) => {
       });
     }
 
-    var statusCode = (studentNewStatus?2:0) + (studentOldStatus?1:0);
-
-    var statusColor = (statusCode === 3) ? ("2ecc71") : ( (statusCode === 0) ? ("e74c3c") : ("f1c40f") );
+    var statusCode = (studentNewStatus.code * 3) + (studentOldStatus.code);
 
     if(statusCode !== lastStatusCode) {
+      var statusColor = Math.max(studentNewStatus.code, studentOldStatus.code) === uonetStatus.STATUS_WORKING ?
+        "2ecc71" : 
+        "f1c40f";
+
+      var studentNewMessage = "";
+
+      if (studentNewStatus.code === uonetStatus.STATUS_WORKING)
+        studentNewMessage = "Wszystko powinno działać poprawnie";
+      else if (studentNewStatus.code === uonetStatus.STATUS_ERROR)
+        studentNewMessage = studentNewStatus.message ?
+          `Błąd: \`${studentNewStatus.message}\`` :
+          `Błąd sprawdzania statusu`;
+      else if (studentNewStatus.code === uonetStatus.STATUS_TECHNICAL_BREAK) {
+        studentNewMessage = "Przerwa techniczna"
+      }
+
+      var studentOldMessage = "";
+
+      if (studentOldStatus.code === uonetStatus.STATUS_WORKING)
+        studentOldMessage = "Wszystko powinno działać poprawnie";
+      else if (studentOldStatus.code === uonetStatus.STATUS_ERROR)
+        studentOldMessage = studentOldStatus.message ?
+          `Błąd: \`${studentOldStatus.message}\`` :
+          `Błąd sprawdzania statusu`;
+      else if (studentOldStatus.code === uonetStatus.STATUS_TECHNICAL_BREAK) {
+        studentOldMessage = "Przerwa techniczna"
+      }
+
       const embed = new Discord.RichEmbed()
-        .setTitle("Status się zmienił!")
+        .setTitle("Status dzienniczka się zmienił!")
         .setColor(statusColor)
-        .addField("Nowy moduł uczeń:", studentNewStatus?"Wszystko powinno działać poprawnie":"Awaria")
-        .addField("Stary moduł uczeń:", studentOldStatus?"Wszystko powinno działać poprawnie":"Awaria");
+        .addField("Nowy moduł uczeń:", studentNewMessage)
+        .addField("Stary moduł uczeń:", studentOldMessage);
       statusChannels.forEach((statusChannel) => {
         statusChannel.send({embed});
       });
