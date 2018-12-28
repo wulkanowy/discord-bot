3a2ca4adb1ae1a3dff2b9158ced2cc6dcf0d41ae
Dominik Korsa <dominik.korsa@gmail.com>
2018-12-28 23:19:47 +0100

    Formatted message.js

--- events/message.js
+++ events/message.js
@@ -1,20 +1,20 @@
 const FuzzySet = require("fuzzyset.js");
 
 module.exports = (client, message) => {
-    if (message.author.bot) return;
-    if (message.content.indexOf(client.config.prefix) !== 0) return;
-    if (message.channel.name !== client.config.channels.bot) return;
+  if (message.author.bot) return;
+  if (message.content.indexOf(client.config.prefix) !== 0) return;
+  if (message.channel.name !== client.config.channels.bot) return;
 
-    var args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
-    var command = args.shift().toLowerCase();
-    var cmd = client.commands.get(command);
+  var args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
+  var command = args.shift().toLowerCase();
+  var cmd = client.commands.get(command);
 
-    if (!cmd) {
-      let commandsFuzzySet = new FuzzySet(Array.from(client.commands.keys()));
-      let match = commandsFuzzySet.get(command, null, 0.5);
-      message.channel.send(`Nie ma takiej komendy \`${client.config.prefix}${command}\`\n${match?`Czy chodziło ci o \`${client.config.prefix}${match[0][1]}\`? *(podobieństwo **${Math.round(match[0][0] * 1000)/10}%**)*\n`:''}W celu uzyskania pomocy wpisz \`!pomoc\``);
-      return;
-    }
-    
-    cmd.run(client, message, args);
-  };
+  if (!cmd) {
+    let commandsFuzzySet = new FuzzySet(Array.from(client.commands.keys()));
+    let match = commandsFuzzySet.get(command, null, 0.5);
+    message.channel.send(`Nie ma takiej komendy \`${client.config.prefix}${command}\`\n${match ? `Czy chodziło ci o \`${client.config.prefix}${match[0][1]}\`? *(podobieństwo **${Math.round(match[0][0] * 1000) / 10}%**)*\n` : ''}W celu uzyskania pomocy wpisz \`!pomoc\``);
+    return;
+  }
+
+  cmd.run(client, message, args);
+};
