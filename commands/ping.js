20a79bd740618740c02c612b5c3adea0d25a2e2b
doteq <dawidwikus@gmail.com>
2019-05-23 18:07:31 +0200

    Use singlequote quotes

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 exports.run = async (client, message) => {
-  const testmessage = await message.channel.send("Sprawdzam...");
+  const testmessage = await message.channel.send('Sprawdzam...');
   const embed = new Discord.RichEmbed()
     .setTitle('PONG! :ping_pong:')
     .addField(`Udało mi się odpowiedzieć w ${testmessage.createdTimestamp - message.createdTimestamp}ms`, `Opóźnienie API wynosi ${client.ping}ms`)
