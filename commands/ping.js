e345fbe025103462bff36250b6f6c173ac90ce68
doteq <dawidwikus@gmail.com>
2019-05-23 21:30:03 +0200

    Round latency (#30)

--- commands/ping.js
+++ commands/ping.js
@@ -4,7 +4,7 @@ exports.run = async (client, message) => {
   const testmessage = await message.channel.send('Sprawdzam...');
   const embed = new Discord.RichEmbed()
     .setTitle('PONG! :ping_pong:')
-    .addField(`Udało mi się odpowiedzieć w ${testmessage.createdTimestamp - message.createdTimestamp}ms`, `Opóźnienie API wynosi ${client.ping}ms`)
+    .addField(`Udało mi się odpowiedzieć w ${Math.round(testmessage.createdTimestamp - message.createdTimestamp)}ms`, `Opóźnienie API wynosi ${Math.round(client.ping)}ms`)
     .setColor('F44336');
   testmessage.edit({ embed });
 };
