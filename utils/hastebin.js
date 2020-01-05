428069e7a644f11d9edfb4e652844760ed565b05
doteq <doteeqq@gmail.com>
2020-01-05 21:37:07 +0100

    Fix ESLint errors

--- utils/hastebin.js
+++ utils/hastebin.js
@@ -1,36 +1,37 @@
 const http = require('https');
+
 exports.run = (client, message) => {
-    var usrmessage = message
-    var code = message.content.slice(2, -2)
-    var options = {
-        "method": "POST",
-        "hostname": "hastebin.cf",
-        "path": "/documents"
-    };
+  const usrmessage = message;
+  const code = message.content.slice(2, -2);
+  const options = {
+    method: 'POST',
+    hostname: 'hastebin.cf',
+    path: '/documents',
+  };
 
-    var req = http.request(options, function(res) {
-        var chunks = [];
+  const req = http.request(options, (res) => {
+    const chunks = [];
 
-        res.on("data", function(chunk) {
-            chunks.push(chunk);
-        });
+    res.on('data', (chunk) => {
+      chunks.push(chunk);
+    });
 
-        res.on("end", function() {
-            var body = JSON.parse(Buffer.concat(chunks).toString());
-            message.delete()
-                .then(msg => message.channel.send({
-                    embed: {
-                        color: 11062,
-                        author: {
-                            name: usrmessage.author.username,
-                            icon_url: usrmessage.author.displayAvatarURL
-                        },
-                        title: "hastebin.cf/" + body.key,
-                        url: "https://hastebin.cf/" + body.key
-                    }
-                }));
-        });
-    })
-    req.write(code);
-    req.end();
-}
+    res.on('end', () => {
+      const body = JSON.parse(Buffer.concat(chunks).toString());
+      message.delete()
+        .then(() => message.channel.send({
+          embed: {
+            color: 11062,
+            author: {
+              name: usrmessage.author.username,
+              icon_url: usrmessage.author.displayAvatarURL,
+            },
+            title: `hastebin.cf/${body.key}`,
+            url: `https://hastebin.cf/${body.key}`,
+          },
+        }));
+    });
+  });
+  req.write(code);
+  req.end();
+};
