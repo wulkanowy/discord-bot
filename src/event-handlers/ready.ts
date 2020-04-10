38c0e16b5ebee960e303477701821cc02dec5983
Dominik Korsa <dominik.korsa@gmail.com>
2020-04-10 21:56:02 +0200

    Add running (watching) for status

--- src/event-handlers/ready.ts
+++ src/event-handlers/ready.ts
@@ -35,5 +35,30 @@ export default function readyHandler(client: Client): void {
     performCheck();
   }, statusCheckInterval);
 
+  client.user?.setActivity('od mniej niż minuty', {
+    type: 'WATCHING',
+  });
+
+  let activeTimeMinutes = 1;
+
+  setInterval(() => {
+    if (activeTimeMinutes >= 60 * 48) {
+      const activeTimeDays = Math.floor(activeTimeMinutes / 60 / 24);
+      client.user?.setActivity(`od ${activeTimeDays} dni`, {
+        type: 'WATCHING',
+      });
+    } if (activeTimeMinutes >= 60) {
+      const activeTimeHours = Math.floor(activeTimeMinutes / 60);
+      client.user?.setActivity(activeTimeHours === 1 ? 'od godziny' : `od ${activeTimeHours} godzin`, {
+        type: 'WATCHING',
+      });
+    } else {
+      client.user?.setActivity(activeTimeMinutes === 1 ? 'od minuty' : `od ${activeTimeMinutes} minut`, {
+        type: 'WATCHING',
+      });
+    }
+    activeTimeMinutes += 1;
+  }, 60000);
+
   console.log('Uruchomiono bota :)');
 }
