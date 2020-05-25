d8e0ee455c9d249701dcf0b0e980bd3ec9ce6135
PanTajemnic <idziejczak3@gmail.com>
2020-05-25 22:07:45 +0200

    Add !status for different roles handling

--- src/commands/status.ts
+++ src/commands/status.ts
@@ -8,11 +8,48 @@ export default async function status(
   args: string[],
 ): Promise<void> {
   message.channel.startTyping();
+  let host: string;
+  let symbol: string;
+  let mobileUrl: string;
+  let expectedTitle: string;
+  if (message.member === null) return;
+  if (message.member.roles.cache.some((role: Discord.Role) => role.name === 'eszkola.opolskie.pl')) {
+    host = 'eszkola.opolskie.pl';
+    symbol = args[0] || 'opole';
+    mobileUrl = 'https://uonetplus-komunikacja.eszkola.opolskie.pl';
+    expectedTitle = 'Logowanie do systemu Opolskiej e-Szkoła';
+  } else if (message.member.roles.cache.some((role: Discord.Role) => role.name === 'edu.gdansk.pl')) {
+    host = 'edu.gdansk.pl';
+    symbol = args[0] || 'gdansk';
+    mobileUrl = 'https://uonetplus-komunikacja.edu.gdansk.pl';
+    expectedTitle = 'Logowanie do systemu e-Szkoła';
+  } else if (message.member.roles.cache.some((role: Discord.Role) => role.name === 'umt.tarnow.pl')) {
+    host = 'umt.tarnow.pl';
+    symbol = args[0] || 'tarnow';
+    mobileUrl = 'https://uonetplus-komunikacja.umt.tarnow.pl';
+    expectedTitle = 'Logowanie do systemu e-Szkoła';
+  } else if (message.member.roles.cache.some((role: Discord.Role) => role.name === 'resman.pl')) {
+    host = 'resman.pl';
+    symbol = args[0] || 'rzeszow';
+    mobileUrl = 'https://uonetplus-komunikacja.resman.pl';
+    expectedTitle = 'Logowanie do systemu';
+  } else if (message.member.roles.cache.some((role: Discord.Role) => role.name === 'edu.lublin.eu')) {
+    host = 'edu.lublin.eu';
+    symbol = args[0] || 'lublin';
+    mobileUrl = 'https://uonetplus-komunikacja.edu.lublin.eu';
+    expectedTitle = 'Logowanie do systemu';
+  } else {
+    host = 'vulcan.net.pl';
+    symbol = args[0] || 'warszawa';
+    mobileUrl = 'https://lekcjaplus.vulcan.net.pl';
+    expectedTitle = 'Uczeń';
+  }
 
-  const symbol = args[0] || 'warszawa';
 
   try {
-    await uonetStatus.sendStatusMessage([message.channel], symbol);
+    await uonetStatus.sendStatusMessage(
+      [message.channel], symbol, undefined, host, mobileUrl, expectedTitle,
+    );
   } catch (error) {
     console.error(error);
     message.channel.send(`Błąd: \`${error.message}\``);
