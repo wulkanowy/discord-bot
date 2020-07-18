b8d341d018aed4e92a9c37b7093691b3733b3272
Dominik Korsa <dominik.korsa@gmail.com>
2020-07-18 22:45:38 +0200

    Migrate getDevBranchBuilds to Got

--- src/utils/app-version/get-dev-branch-builds.ts
+++ src/utils/app-version/get-dev-branch-builds.ts
@@ -1,21 +1,20 @@
-import request from 'request-promise-native';
+import got from 'got';
 import { DevBuild, DevBuildRedirect, getDevBuildBranch } from '.';
 
 export default async function getDevBranchBuilds(): Promise<Array<DevBuild | DevBuildRedirect>> {
   const url = `https://api.github.com/repos/wulkanowy/wulkanowy/branches${
     process.env.GITHUB_API_TOKEN ? `?access_token=${process.env.GITHUB_API_TOKEN}` : ''
   }`;
-  const options = {
-    method: 'GET',
-    uri: url,
+
+  const response = await got<{
+    name: string;
+  }[]>(url, {
     headers: {
       'User-Agent': 'Mozilla/5.0',
     },
-    json: true,
-  };
+    responseType: 'json',
+  });
 
-  const response = await request(options);
-
-  const branchNames = response.map((branch: { name: string }) => branch.name);
+  const branchNames = response.body.map((branch: { name: string }) => branch.name);
   return Promise.all(branchNames.map((e: string) => getDevBuildBranch(e)));
 }
