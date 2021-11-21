d5c414135c8945d84d5416a58575c31eefcb865f
Dawid K <80264606+drobotk@users.noreply.github.com>
2021-11-21 15:57:12 +0100

    Fix crash in guildMemberAddHandler (#64)

--- src/event-handlers/guild-member-add.ts
+++ src/event-handlers/guild-member-add.ts
@@ -9,16 +9,19 @@ export default async function guildMemberAddHandler(
   console.log(`${member.user.username} wbił!`);
   const avatar = member.user.avatarURL();
   const { id } = member.user;
-  const embedDM = new Discord.MessageEmbed()
-    .setTitle('Witaj na serwerze Wulkanowego!')
-    .setDescription('Używamy tutaj specjalnych ról, by móc oznaczyć osoby korzystające z danej wersji e-dziennika (np. `vulcan.net.pl`, `eszkola.opolskie.pl`).\nUłatwia to rozwiązywanie problemów i testowanie nowych funkcji.\n\nBy wyświetlić listę wszystkich ról, przejdź na kanał #choose-role\n - By przydzielić sobie odpowienią rolę, kliknij w odpowiednią reakcję.\n\n**Dziękujemy za wybranie naszej aplikacji!**')
-    .setColor('#F44336');
-  await member.send({ embeds: [embedDM] });
+  if (!member.user.bot) {
+    const embedDM = new Discord.MessageEmbed()
+      .setTitle('Witaj na serwerze Wulkanowego!')
+      .setDescription('Używamy tutaj specjalnych ról, by móc oznaczyć osoby korzystające z danej wersji e-dziennika (np. `vulcan.net.pl`, `eszkola.opolskie.pl`).\nUłatwia to rozwiązywanie problemów i testowanie nowych funkcji.\n\nBy wyświetlić listę wszystkich ról, przejdź na kanał #choose-role\n - By przydzielić sobie odpowienią rolę, kliknij w odpowiednią reakcję.\n\n**Dziękujemy za wybranie naszej aplikacji!**')
+      .setColor('#F44336');
+    await member.send({ embeds: [embedDM] });
+  }
 
   const channel = member.guild.channels.cache.find(
     (ch) => ch.name === client.config.channels.greetings,
   );
   if (!channel || !(channel instanceof Discord.TextChannel)) return;
+  if (!member.guild.me?.permissionsIn(channel).has(['SEND_MESSAGES'])) return;
   const embedPublic = new Discord.MessageEmbed()
     .setAuthor('Na serwerze pojawił się', avatar || undefined)
     .setDescription(`<@!${id}>`)
