27f14bd2aaa2b55d133ddbbbc3b50113946feeed
Mikołaj Pich <m.pich@outlook.com>
2022-08-31 18:45:48 +0200

    Update status domains

--- src/utils/uonet-status/send-status-message.ts
+++ src/utils/uonet-status/send-status-message.ts
@@ -13,14 +13,12 @@ export default async function sendStatusMessage(
   lastStatusCode: number | undefined = undefined,
   host: string | undefined = 'vulcan.net.pl',
   mobileUrl: string | undefined = 'https://lekcjaplus.vulcan.net.pl',
-  expectedTitle: string | undefined = 'Uczeń',
 ): Promise<number> {
-  const [studentNewStatus, studentOldStatus, mobileApiStatus] = await Promise.all([
-    checkService(`https://uonetplus-uczen.${host}/${symbol}`, expectedTitle),
-    checkService(`https://uonetplus-opiekun.${host}/${symbol}`, expectedTitle),
+  const [studentNewStatus, messagesStatus, mobileApiStatus] = await Promise.all([
+    checkService(`https://uonetplus-uczen.${host}/${symbol}`, 'Uczeń'),
+    checkService(`https://uonetplus-wiadomosciplus.${host}/${symbol}`, 'Wiadomości Plus'),
     checkService(`${mobileUrl}/${symbol}`, 'UONET+ dla urządzeń mobilnych'),
   ]);
-  console.log(`https://uonetplus-uczen.${host}/${symbol}`);
 
   const statusCode = (studentNewStatus ? studentNewStatus.code : 0)
     + (mobileApiStatus ? mobileApiStatus.code * 8 : 0)
@@ -29,7 +27,7 @@ export default async function sendStatusMessage(
   if (lastStatusCode === undefined || statusCode !== lastStatusCode) {
     const statusColor = Math.max(
       studentNewStatus?.code || 0,
-      studentOldStatus?.code || 0,
+      messagesStatus?.code || 0,
       mobileApiStatus?.code || 0,
     ) === StatusCode.Working ? '#2ecc71' : '#f1c40f';
 
@@ -37,12 +35,12 @@ export default async function sendStatusMessage(
       .setTitle(`Status dzienniczka ${host} (dla symbolu *${symbol}*)`)
       .setColor(statusColor)
       .addField('Nowy moduł uczeń:', interpretCodeMessage(studentNewStatus))
-      .addField('Stary moduł uczeń:', interpretCodeMessage(studentOldStatus))
+      .addField('Wiadomości plus:', interpretCodeMessage(messagesStatus))
       .addField('API mobilne:', interpretCodeMessage(mobileApiStatus));
 
     if (Math.max(
       studentNewStatus.code,
-      studentOldStatus.code,
+      messagesStatus.code,
       mobileApiStatus.code,
     ) === StatusCode.Working) {
       embed.setImage('https://i.imgur.com/oBPbqmy.png');
