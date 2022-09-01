acd871f378b96e38924177886f7c20d7d20cacb1
Mikołaj Pich <m.pich@outlook.com>
2022-09-01 15:37:24 +0200

    Fix download command result

--- src/utils/app-version/get-dev-build-branch.ts
+++ src/utils/app-version/get-dev-build-branch.ts
@@ -1,5 +1,5 @@
 import got from 'got';
-import { DevBuild, DevBuildRedirect } from '.';
+import {DevBuild, DevBuildRedirect} from '.';
 
 export default async function getDevBuildBranch(
   branch: string,
@@ -8,19 +8,21 @@ export default async function getDevBuildBranch(
     const url = `https://manager.wulkanowy.net.pl/v1/build/app/daeff1893f3c8128/branch/${branch}`;
 
     const response = await got<{
-      build_number: number;
-      build_slug: string;
-      artifact_url: string;
-      finished_at: string;
+      data: {
+        build_number: number;
+        build_slug: string;
+        artifact_url: string;
+        finished_at: string;
+      }
     }>(url, {
       responseType: 'json',
     });
 
     return {
       branch,
-      url: `https://manager.wulkanowy.net.pl/v1/download/app/daeff1893f3c8128/build/${response.body.build_slug}/artifact/${response.body.artifact_url}`,
-      version: response.body.build_number,
-      publishedAt: response.body.finished_at,
+      url: `https://manager.wulkanowy.net.pl/v1/download/app/daeff1893f3c8128/build/${response.body.data.build_slug}/artifact/${response.body.data.artifact_url}`,
+      version: response.body.data.build_number,
+      publishedAt: response.body.data.finished_at,
       redirect: false,
     };
   } catch (error) {
